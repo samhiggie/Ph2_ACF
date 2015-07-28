a4e8c0fe03800a2cb44d254942050fa868d8db9a
G. Auzinger <georg.auzinger@cern.ch>
2015-07-28 16:48:22 +0200

    fixing setup.sh & RootWeb makefile

--- setup.sh
+++ setup.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
+
+#CACTUS
+export CACTUSBIN=/opt/cactus/bin
+export CACTUSLIB=/opt/cactus/lib
+export CACTUSINCLUDE=/opt/cactus/include
+
+# QT if installed
 export QTDIR=/usr/local/Trolltech/Qt-4.8.5
 export QMAKESPEC=
-# export ROOTSYS=/usr/local/qtRoot/root
 export QTROOTSYSDIR=/usr/local/qtRoot/root
 
-export PATH=$QTDIR/bin:~/Ph2_ACF/bin:$PATH
-export LD_LIBRARY_PATH=/opt/cactus/lib:$QTROOTSYSDIR/lib:$QTDIR/lib:~/Ph2_ACF/lib:${LD_LIBRARY_PATH}
+# BOOST
+export BOOST_LIB=/opt/cactus/lib/boost
+export BOOST_INCLUDE=/opt/cactus/include/boost
+
+# QT if installed
+export QTDIR=/usr/local/Trolltech/Qt-4.8.5
+export QMAKESPEC=
+export QTROOTSYSDIR=/usr/local/qtRoot/root
+
+#ROOT
+source /usr/local/bin/thisroot.sh
+
+#Ph2_ACF
+export BASE_DIR=$(pwd)
+
+export PATH=$QTDIR/bin:$BASE_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$BASE_DIR/RootWeb/lib:/opt/cactus/lib:$QTROOTSYSDIR/lib:$QTDIR/lib:$BASE_DIR/lib:${LD_LIBRARY_PATH}
