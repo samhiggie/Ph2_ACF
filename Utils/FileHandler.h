a854591876323abefb5b6e70b8ae16fea00f6017
Andrea <andrea.massetti@cern.ch>
2015-08-04 12:40:47 +0200

    fixed segmantation fault in multithrading

--- Utils/FileHandler.h
+++ Utils/FileHandler.h
@@ -19,6 +19,8 @@ class FileHandler
 	std::thread fThread;
 	std::mutex fMutex;
 	bool fFileIsOpened ;
+	// std::vector<uint32_t>* fData;
+
   public:
 	FILE* fBinaryFile;
 	//Constructor
@@ -33,30 +35,42 @@ class FileHandler
 		closeFile();
 	}
 
-	inline std::string getFilename() {
+	std::string getFilename() {
 		return fBinaryFileName;
 	}
 
-	inline bool openFile( std::string pOption ) {
+	bool openFile( std::string pOption ) {
 		if ( !file_open() ) {
 			// if ( !boost::filesystem::exists( getFilename() + ".raw" ) )
+			fMutex.lock();
 			fBinaryFile = fopen( ( getFilename() + ".raw" ).c_str(), pOption.c_str() );
+			fMutex.unlock();
 			fFileIsOpened = true;
 		}
 		return file_open();
 	}
 
-	inline int closeFile() {
-		return fclose( fBinaryFile );
+	int closeFile() {
+		fMutex.lock();
+		std::cout << "Closing binary file!" << std::endl;
+		int cStatus = fclose( fBinaryFile );
+		fMutex.unlock();
+		return cStatus;
 	}
 
-	inline bool file_open() {
+	bool file_open() {
 		return fFileIsOpened;
 	}
 
-	inline void write( std::vector<uint32_t > pData ) {
+	void write( std::vector<uint32_t > pData ) {
+		// std::vector<uint32_t> temp = pData;
+		// char cBuffer[temp.size() * 4];
+		// std::copy( temp.begin(), temp.end(), cBuffer );
+		// std::cout << "Done with Deep Copy" << std::endl;
+		// std::vector<uint32_t> cTmpData = pData;
 		fThread = std::thread( &FileHandler::writeFile, this, pData );
-		fThread.detach();
+		fThread.join();
+		std::cout << "threads joined " << std::endl;
 	}
 
 	inline const std::vector<uint32_t> read() {
@@ -64,15 +78,20 @@ class FileHandler
 	}
 
   private:
-	inline void write( std::vector<uint32_t>  pData ) {
+	void writeFile( std::vector<uint32_t> pData ) {
+		// std::cout << "New Thread! " << std::endl;
+		// std::mutex pMutex;
+		// pMutex.lock();
+		// std::vector<uint32_t> temp = pData;
+		// pMutex.unlock();
 
-		fMutex.lock();
+		std::vector<uint32_t> cTmpData = pData;
 		char cBuffer[pData.size() * 4];
 
 		std::copy( pData.begin(), pData.end(), cBuffer );
-		fMutex.unlock();
+		fMutex.lock();
 		fwrite( cBuffer  , sizeof( cBuffer ) , 1, fBinaryFile );
-		closeFile();
+		fMutex.unlock();
 	}
 };
 
