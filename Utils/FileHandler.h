85e2c434827799fecf3685c9c16c69356a0621fc
Andrea <andrea.massetti@cern.ch>
2015-08-06 11:06:02 +0200

    working version, fixed write file bug

--- Utils/FileHandler.h
+++ Utils/FileHandler.h
@@ -22,21 +22,20 @@ class FileHandler
 	bool fFileIsOpened ;
 	bool is_set;
 
-	// std::vector<uint32_t>* fData;
 
   public:
-	//FILE* fBinaryFile;
+
 	std::fstream fBinaryFile;
 	std::vector<uint32_t> fData;
 	//Constructor
-	FileHandler( std::string cBinaryFileName  /*std::string cOption*/ ):
+	FileHandler( std::string cBinaryFileName ):
 
 		fBinaryFileName( cBinaryFileName ),
 		fFileIsOpened( false ) ,
 		is_set( false ) {
 		openFile( );
 		fThread = std::thread( &FileHandler::writeFile, this );
-		fThread.detach();
+		// fThread.detach();
 
 	}
 
@@ -73,8 +72,9 @@ class FileHandler
 	bool openFile( ) {
 		if ( !file_open() ) {
 			// if ( !boost::filesystem::exists( getFilename() + ".raw" ) )
+			// 	remove( ( getFilename() + ".raw" ).c_str() );
 			fMutex.lock();
-			fBinaryFile.open( ( getFilename() + ".raw" ).c_str(), std::fstream::in | std::fstream::out | std::fstream::binary );
+			fBinaryFile.open( ( getFilename() + ".raw" ).c_str(), std::fstream::trunc | std::fstream::in | std::fstream::out | std::fstream::binary );
 			std::cout << "File opened!" << std::endl;
 			fMutex.unlock();
 			fFileIsOpened = true;
@@ -95,52 +95,21 @@ class FileHandler
 		return fFileIsOpened;
 	}
 
-	// void write( std::vector<uint32_t > pData ) {
-	// 	fThread = std::thread( &FileHandler::writeFile, this, pData );
-	// 	fThread.join();
-	// }
 
-	// inline  std::vector<uint32_t> read() {
-
-
-
-
-	// 	fseek( fBinaryFile, 0, SEEK_END );
-
-	// 	std::cout << "check1" << std::endl;
-	// 	long lsize = ftell( fBinaryFile );
-	// 	rewind( fBinaryFile );
-	// 	std::cout << "check2" << std::endl;
-
-	// 	lsize /= sizeof( uint32_t );
-	// 	uint32_t pDataBuffer[lsize];
-	// 	//read file
-	// 	fread( pDataBuffer, sizeof( uint32_t ), lsize, fBinaryFile );
-	// 	std::vector<uint32_t> cVector( lsize );
-	// 	for ( int i = 0; i < lsize; i++ )
-	// 		cVector[i] = pDataBuffer[i];
-
-	// 	return cVector;
-
-	// }
 
   private:
 	void writeFile() {
 
 		while ( true ) {
-			//std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
+
 			if ( is_set ) {
 				fMutex.lock();
 				std::cout << "is_set = true: writing..." << std::endl;
 				fMutex.unlock();
-
 				fMutex.lock();
-				//for ( auto& cElements : fData )
-				//fBinaryFile.write( ( char* ) &cElements, sizeof( uint32_t ) );
-				// std::copy( fData.begin(), fData.end(), std::ostreambuf_iterator<char>( fBinaryFile ) );
 				uint32_t cBuffer[fData.size()];
 				std::copy( fData.begin(), fData.end(), cBuffer );
-				fBinaryFile.write( ( const char* )cBuffer, sizeof( cBuffer ) );
+				fBinaryFile.write( ( char* )&cBuffer, sizeof( cBuffer ) );
 				fData.clear();
 				is_set = false;
 				fMutex.unlock();
@@ -148,26 +117,12 @@ class FileHandler
 			}
 			else {
 				fMutex.lock();
-				// std::cout << "is_set = false: NOT writing..." << std::endl;
 				fMutex.unlock();
 				continue;
 			}
-			// if ( !is_set ) {
-			// 	fMutex.lock();
-			// 	std::cout << "is_set = false: NOT writing..." << std::endl;
-			// 	fMutex.unlock();
-			// }
-			// std::vector<uint32_t> cVectorCopy = pData;
-			// uint32_t pDataBuffer[cVectorCopy.size()];
-			// std::copy( cVectorCopy.begin(), cVectorCopy.end(), pDataBuffer );
-			// fMutex.lock();
-			// fwrite( &pDataBuffer , sizeof( pDataBuffer ) , 1, fBinaryFile );
-			// fMutex.unlock();
+
 		}
-		// while ( true );
-		fMutex.lock();
-		std::cout << "What is wrong " << std::endl;
-		fMutex.unlock();
+
 	}
 };
 
