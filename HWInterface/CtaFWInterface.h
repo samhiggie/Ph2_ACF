9dd9b231284bbcfb75e789cb4eb3f28d9565451e
Georg Auzinger <schurl@pothead.dyndns.cern.ch>
2016-01-07 13:41:26 +0100

    adding features for Cta Fpga FW download from SD that went missing during latest merge

--- HWInterface/CtaFWInterface.h
+++ HWInterface/CtaFWInterface.h
@@ -190,6 +190,8 @@ public:
     void FlashProm( const std::string& strConfig, const char* pstrFile );
     /*! \brief Jump to an FPGA configuration */
     void JumpToFpgaConfig( const std::string& strConfig);
+
+    void DownloadFpgaConfig( const std::string& strConfig, const std::string& strDest );
     /*! \brief Is the FPGA being configured ?
      * \return FPGA configuring process or NULL if configuration occurs */
     const FpgaConfig* getConfiguringFpga()
