16a53324296d1b077ac2ced82ca1df9263775a2d
G. Auzinger <georg.auzinger@cern.ch>
2017-02-06 18:16:20 +0100

    added script to run cbc3irrad cyclic in tmux session; added skeleton for monitoring page using jsroot

---
+++ run_irradtest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SESSION_NAME=irradtest
+BASE_DIR=$(pwd)
+
+tmux list-session 2>&1 | grep -q "^$SESSION_NAME" || tmux new-session -s $SESSION_NAME -d
+tmux send-keys -t $SESSION_NAME "cd $BASE_DIR" Enter
+
+tmux send-keys -t $SESSION_NAME ". $BASE_DIR/setup.sh" Enter
+tmux send-keys -t $SESSION_NAME "echo 'Running cbc3irradiation test'" Enter
+tmux send-keys -t $SESSION_NAME "while true; do
+    $BASE_DIR/bin/cbc3irrad -b | tee $BASE_DIR/consoledump.log
+    echo 'bin/cbc3irrad finished with exit code $?. Respawning...' | tee $BASE_DIR/consoledump.log
+    sleep 60
+done" Enter
