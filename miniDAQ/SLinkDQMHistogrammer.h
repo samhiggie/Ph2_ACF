5a38ab5ae7597ee35d4327c5be9c29efa75d76e4
G. Auzinger <georg.auzinger@cern.ch>
2017-11-17 17:15:03 +0100

    first try at an integrated DQM in miniDAQ - this needs to be thoroughly tested

--- miniDAQ/SLinkDQMHistogrammer.h
+++ miniDAQ/SLinkDQMHistogrammer.h
@@ -45,14 +45,15 @@ class SLinkDQMHistogrammer
     /*!
      * Book histograms
      */
-     void bookHistograms(const std::vector<std::pair<uint8_t, std::vector<uint8_t> > >& fe_mapping);
-     void bookFEUHistograms(uint8_t fe_id, uint8_t n_rou);
-     void bookROUHistograms(uint8_t fe_id, uint8_t i_rou);
+    void bookHistograms (const std::vector<std::pair<uint8_t, std::vector<uint8_t> > >& fe_mapping);
+    void bookFEUHistograms (uint8_t fe_id, uint8_t n_rou);
+    void bookROUHistograms (uint8_t fe_id, uint8_t i_rou);
 
     /*!
      * Fill histograms
      */
-    void fillHistograms(const std::vector<DQMEvent*>& event_list);
+    void fillHistograms (const std::vector<DQMEvent*>& event_list);
+    void fillHistograms (const DQMEvent* event);
     void saveHistograms (const std::string& out_file);
     void resetHistograms();
     void fillEventTrendHisto (TH1I* th, unsigned long ival, unsigned int val);
@@ -70,65 +71,65 @@ class SLinkDQMHistogrammer
     std::vector<unsigned int>* dut0C1chData_;
     std::vector<unsigned int>* dut1C0chData_;
     std::vector<unsigned int>* dut1C1chData_;
-    struct ROUHistos 
+    struct ROUHistos
     {
-      TH1I* l1ACounterH_;
-      TH1I* errBitH_;
-      TH1I* plAddH_;
-      TH1I* evenChanOccuH_;
-      TH1I* oddChanOccuH_;
-      TProfile* hitVsVCTHH_;
-      bool bookedHistos;
-      uint32_t vcthSetting_;
+        TH1I* l1ACounterH_;
+        TH1I* errBitH_;
+        TH1I* plAddH_;
+        TH1I* evenChanOccuH_;
+        TH1I* oddChanOccuH_;
+        TProfile* hitVsVCTHH_;
+        bool bookedHistos;
+        uint32_t vcthSetting_;
     };
 
-    struct FEUHistos 
+    struct FEUHistos
     {
-      TH2I* hitCorrC0H_;
-      TH2I* hitCorrC1H_;
-      TH1D* deltaHitC0H;
-      TH1D* deltaHitC1H;
-      
-      TH2I* sen0HitProfH_;
-      TH2I* sen1HitProfH_;
-      
-      TH1I* sen0HitProfUnfoldedH_;
-      TH1I* sen1HitProfUnfoldedH_;
-      
-      TH1I* sen0C0HitProfH_;
-      TH1I* sen0C1HitProfH_;
-      TH1I* sen1C0HitProfH_;
-      TH1I* sen1C1HitProfH_;
-      
-      TH1I* L1CounterDiffH_;
-      TH1I* PLAddPhaseDiffH_;
-      TH2I* PLAddPhaseCorrH_;
-      TH1I* rouErrorH_;
+        TH2I* hitCorrC0H_;
+        TH2I* hitCorrC1H_;
+        TH1D* deltaHitC0H;
+        TH1D* deltaHitC1H;
 
-      TH1I* senCorrH_;
-      TH1I* stubCountH_;
-      TH1F* stubPositionH_;
-      TH1I* stubBendH_;
+        TH2I* sen0HitProfH_;
+        TH2I* sen1HitProfH_;
 
-      TProfile* stubVsHVH_;
+        TH1I* sen0HitProfUnfoldedH_;
+        TH1I* sen1HitProfUnfoldedH_;
 
-      uint8_t nROUs_;
-      uint8_t nColumns_;
-      bool bookedHistos;
-      uint32_t hvSetting_;
+        TH1I* sen0C0HitProfH_;
+        TH1I* sen0C1HitProfH_;
+        TH1I* sen1C0HitProfH_;
+        TH1I* sen1C1HitProfH_;
+
+        TH1I* L1CounterDiffH_;
+        TH1I* PLAddPhaseDiffH_;
+        TH2I* PLAddPhaseCorrH_;
+        TH1I* rouErrorH_;
+
+        TH1I* senCorrH_;
+        TH1I* stubCountH_;
+        TH1F* stubPositionH_;
+        TH1I* stubBendH_;
+
+        TProfile* stubVsHVH_;
+
+        uint8_t nROUs_;
+        uint8_t nColumns_;
+        bool bookedHistos;
+        uint32_t hvSetting_;
     };
 
     std::map <uint16_t, ROUHistos > rouHMap_;
-    std::map <uint8_t, FEUHistos > feuHMap_;    
+    std::map <uint8_t, FEUHistos > feuHMap_;
 
     TH1I* l1AcceptH_;
     TH1I* totalNumberHitsH_;
     TH1I* totalNumberStubsH_;
 
-    void fillFEUHistograms (FEUHistos& feu_h, std::vector<uint8_t>& even_list, std::vector<uint8_t>& odd_list, 
-			   std::vector<uint8_t>& errs, std::vector<uint16_t>& adds, std::vector<uint16_t>& counts);
+    void fillFEUHistograms (FEUHistos& feu_h, std::vector<uint8_t>& even_list, std::vector<uint8_t>& odd_list,
+                            std::vector<uint8_t>& errs, std::vector<uint16_t>& adds, std::vector<uint16_t>& counts);
     void fillROUHistograms (ROUHistos& rou_h,  uint8_t ch);
-    void fillStubInformation(FEUHistos& feu_h, const std::vector<StubInfo>& stubs);
-    void readConditionData(const DQMEvent* evt);
+    void fillStubInformation (FEUHistos& feu_h, const std::vector<StubInfo>& stubs);
+    void readConditionData (const DQMEvent* evt);
 };
 #endif
--- miniDAQ/miniSLinkDQM.cc
+++ miniDAQ/miniSLinkDQM.cc
@@ -23,77 +23,72 @@
 using namespace CommandLineProcessing;
 INITIALIZE_EASYLOGGINGPP
 
-void tokenize ( const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters )
+void dumpEvents (const std::vector<DQMEvent*>& elist, size_t evt_limit, std::ostream& os)
 {
-    // Skip delimiters at beginning.
-    std::string::size_type lastPos = str.find_first_not_of ( delimiters, 0 );
-
-    // Find first "non-delimiter".
-    std::string::size_type pos = str.find_first_of ( delimiters, lastPos );
-
-    while ( std::string::npos != pos || std::string::npos != lastPos )
+    for ( int i = 0; i < std::min (elist.size(), evt_limit); ++i)
     {
-        // Found a token, add it to the vector.
-        tokens.push_back ( str.substr ( lastPos, pos - lastPos ) );
-
-        // Skip delimiters.  Note the "not_of"
-        lastPos = str.find_first_not_of ( delimiters, pos );
-
-        // Find next "non-delimiter"
-        pos = str.find_first_of ( delimiters, lastPos );
+        os << "Event index: " << i + 1 << std::endl;
+        const DQMEvent* ev = elist.at (i);
+        os << *ev << std::endl;
     }
 }
+void readSLinkFromFile (const std::string& filename, std::vector<DQMEvent*>& evList)
+{
+    // open the binary file
+    std::ifstream fh (filename, std::fstream::in | std::fstream::binary);
 
-void dumpEvents (const std::vector<DQMEvent*>& elist, size_t evt_limit, std::ostream& os) {
-  for ( int i = 0; i < std::min(elist.size(), evt_limit); ++i) {
-    os << "Event index: " << i + 1 << std::endl;
-    const DQMEvent* ev = elist.at(i);
-    os << *ev << std::endl;
-  }
-}
-void readSLinkFromFile(const std::string& filename, std::vector<DQMEvent*>& evList) {
-  // open the binary file
-  std::ifstream fh(filename, std::fstream::in | std::fstream::binary);
-  if (!fh) {
-    std::cerr << "Error opening file " << filename << "!" << std::endl;
-    return;
-  }
-
-  evList.clear();
-  // read line-by-line
-  while (1) {
-    std::vector<uint64_t> cData;
-    bool trailerFound = false;
-    while (!fh.eof()) {
-      char buffer[8];
-      fh.read(buffer, sizeof (uint64_t));
-      if (!fh) {
-	std::cerr << "Error reading data from file " << filename << "!" << std::endl;
-	//continue;
-      }
-      uint64_t word;
-      //fh.read ( (char*) &word, sizeof (uint64_t) );
-      std::memcpy(&word, buffer, sizeof (uint64_t));
-      uint64_t correctedWord = (word & 0xFFFFFFFF) << 32 | (word >> 32) & 0xFFFFFFFF;
-      cData.push_back(correctedWord);
-      
-      // Now find the last word of the event
-      if ((correctedWord & 0xFF00000000000000) >> 56 == 0xA0 && 
-	  (correctedWord & 0x00000000000000F0) >> 4  == 0x7)  { // SLink Trailer
-	trailerFound = true;
-        break; 
-      }
+    if (!fh)
+    {
+        std::cerr << "Error opening file " << filename << "!" << std::endl;
+        return;
     }
 
-    // 3 is the minimum size for an empty SLinkEvent (2 words header and 1 word trailer)
-    if (trailerFound && cData.size() > 3) {
-      DQMEvent* ev = new DQMEvent(new SLinkEvent(cData));
-      evList.push_back(ev);
+    evList.clear();
+
+    // read line-by-line
+    while (1)
+    {
+        std::vector<uint64_t> cData;
+        bool trailerFound = false;
+
+        while (!fh.eof() )
+        {
+            char buffer[8];
+            fh.read (buffer, sizeof (uint64_t) );
+
+            if (!fh)
+            {
+                std::cerr << "Error reading data from file " << filename << "!" << std::endl;
+                //continue;
+            }
+
+            uint64_t word;
+            //fh.read ( (char*) &word, sizeof (uint64_t) );
+            std::memcpy (&word, buffer, sizeof (uint64_t) );
+            uint64_t correctedWord = (word & 0xFFFFFFFF) << 32 | (word >> 32) & 0xFFFFFFFF;
+            cData.push_back (correctedWord);
+
+            // Now find the last word of the event
+            if ( (correctedWord & 0xFF00000000000000) >> 56 == 0xA0 &&
+                    (correctedWord & 0x00000000000000F0) >> 4  == 0x7)    // SLink Trailer
+            {
+                trailerFound = true;
+                break;
+            }
+        }
+
+        // 3 is the minimum size for an empty SLinkEvent (2 words header and 1 word trailer)
+        if (trailerFound && cData.size() > 3)
+        {
+            DQMEvent* ev = new DQMEvent (new SLinkEvent (cData) );
+            evList.push_back (ev);
+        }
+
+        if (fh.eof() ) break;
     }
-    if (fh.eof()) break;
-  }
-  // time to close the file
-  if (fh.is_open()) fh.close();
+
+    // time to close the file
+    if (fh.is_open() ) fh.close();
 }
 int main ( int argc, char* argv[] )
 {
@@ -131,6 +126,7 @@ int main ( int argc, char* argv[] )
     cmd.defineOptionAlternative ( "skipDebugHist", "g" );
 
     int result = cmd.parse ( argc, argv );
+
     if ( result != ArgvParser::NoParserError )
     {
         std::cerr << cmd.parseErrorDescription ( result );
@@ -139,6 +135,7 @@ int main ( int argc, char* argv[] )
 
     // now query the parsing results
     std::string daqFilename = ( cmd.foundOption ( "file" ) ) ? cmd.optionValue ( "file" ) : "";
+
     if ( daqFilename.empty() )
     {
         std::cerr << "Error, no binary SLink64 .daq format file provided. Quitting" ;
@@ -159,19 +156,19 @@ int main ( int argc, char* argv[] )
 
     // read .daq file and build DQMEvents
     std::vector<DQMEvent*> evList;
-    readSLinkFromFile(daqFilename, evList);
+    readSLinkFromFile (daqFilename, evList);
 
     // either prepare and public DQM histograms or simply dump (a few) events in raw and human readable formats
     if ( cDQMPage )
     {
         gROOT->SetBatch ( true );
-	std::unique_ptr<SLinkDQMHistogrammer> dqmH = std::unique_ptr<SLinkDQMHistogrammer>(new SLinkDQMHistogrammer(0));
- 
-        // book histograms
-        dqmH->bookHistograms(evList[0]->trkPayload().feReadoutMapping());
+        std::unique_ptr<SLinkDQMHistogrammer> dqmH = std::unique_ptr<SLinkDQMHistogrammer> (new SLinkDQMHistogrammer (0) );
 
-        // fill 
-        dqmH->fillHistograms(evList);
+        // book histograms
+        dqmH->bookHistograms (evList[0]->trkPayload().feReadoutMapping() );
+
+        // fill
+        dqmH->fillHistograms (evList);
 
         // save and publish
         // Create the DQM plots and generate the root file
@@ -185,10 +182,11 @@ int main ( int argc, char* argv[] )
         tokenize ( fname, tokens, "." );
         std::string runLabel = tokens[0];
         std::string dqmFilename =  runLabel + "_dqm.root";
-        dqmH->saveHistograms(dqmFilename);
+        dqmH->saveHistograms (dqmFilename);
 
         // find the folder (i.e DQM page) where the histograms will be published
         std::string cDirBasePath;
+
         if ( cmd.foundOption ( "output" ) )
         {
             cDirBasePath = cmd.optionValue ( "output" );
@@ -201,10 +199,12 @@ int main ( int argc, char* argv[] )
         RootWeb::makeDQMmonitor ( dqmFilename, cDirBasePath, runLabel );
         LOG (INFO) << "Saving root file to " << dqmFilename << " and webpage to " << cDirBasePath ;
     }
-    else {
+    else
+    {
         std::stringstream outp;
         dumpEvents ( evList, maxevt, outp );
         LOG (INFO) << outp.str();
     }
+
     return 0;
 }
