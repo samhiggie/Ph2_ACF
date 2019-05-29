3bca00e0358667396fc43fdff71721a51efdd255
Alexander Droll <alexander.droll@cern.ch>
2019-05-29 14:01:02 +0200

    Test Pulse Calibration: new test to determine the gain to convert VCth into electrons, using test pulses.

---
+++ tools/TPCalibration.h
@@ -0,0 +1,40 @@
+#ifndef TPCalibration_h__
+#define TPCalibration_h__
+
+#include "Tool.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TH1F.h"
+
+#include "PedeNoise.h"
+
+using namespace Ph2_HwDescription;
+using namespace Ph2_HwInterface;
+using namespace Ph2_System;
+
+
+class TPCalibration : public Tool
+{
+private: //attributes
+  int fStartAmp;
+  int fEndAmp;
+  int fStepsize;
+  PedeNoise* fPedeNoise;
+  int fTPCount;
+
+public: //methods
+  TPCalibration();
+  ~TPCalibration();
+
+
+  void Init(int pStartAmp, int pEndAmp, int pStepsize);
+  void RunCalibration();
+  void SaveResults();
+  float ConvertAmpToElectrons(float pTPAmp, bool pOffset);
+
+private: //methods
+  void FillHistograms(int pTPAmp);
+  void FitCorrelations();
+};
+
+#endif
